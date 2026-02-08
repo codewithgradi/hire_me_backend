eae5f8386921c7b276880de9f91b0067c306178c
Gradi Puata <gradipuata@gmail.com>
2026-02-08 18:12:18 +0200

    Added urls on http file

--- HireMe.Application/obj/Debug/net10.0/HireMe.Application.AssemblyInfo.cs
+++ HireMe.Application/obj/Debug/net10.0/HireMe.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HireMe.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+611e5a39d7b3d33a2b534d43853ad3914637214c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ada0c836d8f3d8d5409bc5081318afb023bc6b5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HireMe.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HireMe.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
